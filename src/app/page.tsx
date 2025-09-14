ccef88a3ccc25b8fe6c18f9588ae6b00f81f033d
Aashish Kumar Singh <hiaashish10@gmail.com>
2025-09-14 13:13:29 +0530

    04 backgroundjob aetup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
-
-import React from 'react'
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client"
+import { Button } from '@/components/ui/button'
+import { useTRPC } from '@/trpc/client'
+import { useMutation } from '@tanstack/react-query'
+import React, { use } from 'react'
+import { toast } from 'sonner'
 
 const page =  () => {
   
- console.log("server")
+const trpc = useTRPC();
+
+const invoke = useMutation(trpc.invoke.mutationOptions({
+  onSuccess:()=>{
+    toast.success("Event Sent")
+  }
+}));
   return (
-    <div >
-     
+    <div className='p-4 mx-auto'>
+     <Button onClick={()=>invoke.mutate({text:"Ashish"})}
+     >Invoke</Button>
     </div>
   )
 }
